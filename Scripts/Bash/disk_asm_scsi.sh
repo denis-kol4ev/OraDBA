96cec8738adaac47e016208e115565977f63baba
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2020-11-08 17:12:41 +0300

    Update disk_asm_scsi.sh

--- Scripts/Bash/disk_asm_scsi.sh
+++ Scripts/Bash/disk_asm_scsi.sh
@@ -2,7 +2,7 @@
 # matching disk devices to ASM labels and scsi numbers
 
 printf "%s\t%s\t%s\n" "Disk" "ASM Label" "[H:B:T:L]"
-for i in $(lsblk -f | grep oracleasm | awk '{print $1";"$3}' | sort); 
+for i in $(lsblk -f | grep oracleasm |  sort -k3 | awk '{gsub(/[[:punct:]]|[[:digit:]]/, "", $1)}; {print $1";"$3}'); 
 do 
 v_dev=$(echo $i | awk -F ";" '{print $1}');
 v_asm=$(echo $i | awk -F ";" '{print $2}');
