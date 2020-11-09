8bc203505c839c88a15e1125587e05ed300c17cc
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2020-11-09 09:53:35 +0300

    Update server_spec.sh

--- Scripts/Bash/server_spec.sh
+++ Scripts/Bash/server_spec.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # get basic server specification
 
+host
 lscpu | egrep "^CPU\(s\)" | awk '{print "CPU: " $2}'
-
 free -h | grep Mem | awk '{print "RAM: " $2}'
-
 #lsblk | grep disk | awk '{print $1, $4, $6}' | sort -k2 -n
-
 lsblk -b | grep disk | awk '{sum += $4;} END {print "HDD: " sum/1024/1024/1024 "G"}'
