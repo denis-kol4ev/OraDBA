d647579fda9041708f5b83a8a57e6531da421901
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2021-10-20 13:08:43 +0300

    Update booleanExample.sql

--- Scripts/Oracle/Snippets/booleanExample.sql
+++ Scripts/Oracle/Snippets/booleanExample.sql
@@ -13,3 +13,16 @@ begin
     dbms_output.put_line('odd');
    end if;
 end;
+
+--pl/sql boolean example 2
+CREATE OR REPLACE FUNCTION valid_deptid(p_deptid IN departments.department_id%TYPE)
+  RETURN BOOLEAN IS
+  v_dummy PLS_INTEGER;
+BEGIN
+  SELECT 1 INTO v_dummy FROM departments WHERE department_id = p_deptid;
+  RETURN TRUE;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    RETURN FALSE;
+END valid_deptid;
+/
