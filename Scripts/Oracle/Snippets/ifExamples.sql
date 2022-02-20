b961a97acfe44f2f936efdd9e002499e33d7de93
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2022-02-20 16:56:26 +0300

    1

--- Scripts/Oracle/Snippets/ifExamples.sql
+++ Scripts/Oracle/Snippets/ifExamples.sql
@@ -5,3 +5,41 @@ IF THEN ELSE END IF;
 IF THEN ELSIF ELSE END IF;
 */
 
+-- IF THEN END IF;
+declare
+  v1 number := 100;
+  v2 number := 99;
+begin
+  if v1 > v2 then
+    dbms_output.put_line(v1 || ' is greater than ' || v2);
+  end if;
+end;
+/
+
+-- IF THEN ELSE END IF;
+declare
+  v1 number := 100;
+  v2 number := 99;
+begin
+  if v1 > v2 then
+    dbms_output.put_line(v1 || ' is greater than ' || v2);
+  else
+    dbms_output.put_line(v1 || ' is not greater than ' || v2);
+  end if;
+end;
+/
+
+-- IF THEN ELSIF ELSE END IF;
+declare
+  v1 number := 100;
+  v2 number := 99;
+begin
+  if v1 > v2 then
+    dbms_output.put_line(v1 || ' is greater than ' || v2);
+  elsif v1 < v2 then
+    dbms_output.put_line(v1 || ' is lower than ' || v2);
+  else
+    dbms_output.put_line(v1 || ' and ' || v2 || ' are equal');
+  end if;
+end;
+/
