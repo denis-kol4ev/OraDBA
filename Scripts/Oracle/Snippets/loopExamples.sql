30851d03216fb63c1b44c9db8566a1047ff58d05
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2022-03-20 13:06:33 +0300

    Update loopExamples.sql

--- Scripts/Oracle/Snippets/loopExamples.sql
+++ Scripts/Oracle/Snippets/loopExamples.sql
@@ -11,10 +11,9 @@ insert into sales_data values(2003, 2300);
 insert into sales_data values(2004, 3100);
 commit;
 
-CREATE OR REPLACE PROCEDURE display_total_sales (year_in IN PLS_INTEGER)
-IS
+CREATE OR REPLACE PROCEDURE display_total_sales(year_in IN PLS_INTEGER) IS
 BEGIN
-   DBMS_OUTPUT.put_line ('Total for year ' || year_in);
+  DBMS_OUTPUT.put_line('Total for year ' || year_in);
 END display_total_sales;
 /
 
@@ -22,18 +21,15 @@ END display_total_sales;
 Простой цикл начинается с ключевого слова LOOP и завершается командой END LOOP.
 Выполнение цикла прерывается при выполнении команды EXIT WHEN.
 */
-CREATE OR REPLACE PROCEDURE display_multiple_years (
-   start_year_in IN PLS_INTEGER
- , end_year_in   IN PLS_INTEGER
-)
-IS
-   l_current_year   PLS_INTEGER := start_year_in;
+CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
+                                                   end_year_in   IN PLS_INTEGER) IS
+  l_current_year PLS_INTEGER := start_year_in;
 BEGIN
-   LOOP
-      EXIT WHEN l_current_year > end_year_in;
-      display_total_sales (l_current_year);
-      l_current_year := l_current_year + 1;
-   END LOOP;
+  LOOP
+    EXIT WHEN l_current_year > end_year_in;
+    display_total_sales(l_current_year);
+    l_current_year := l_current_year + 1;
+  END LOOP;
 END display_multiple_years;
 /
 
@@ -41,18 +37,14 @@ END display_multiple_years;
 Цикл WHILE имеет много общего с простым циклом. Принципиальное отличие заключается в том, 
 что условие завершения проверяется перед выполнением очередной итерации.
 */
-CREATE OR REPLACE PROCEDURE display_multiple_years (
-   start_year_in IN PLS_INTEGER
- , end_year_in   IN PLS_INTEGER
-)
-IS
-   l_current_year   PLS_INTEGER := start_year_in;
+CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
+                                                   end_year_in   IN PLS_INTEGER) IS
+  l_current_year PLS_INTEGER := start_year_in;
 BEGIN
-   WHILE (l_current_year <= end_year_in)
-   LOOP
-      display_total_sales (l_current_year);
-      l_current_year := l_current_year + 1;
-   END LOOP;
+  WHILE (l_current_year <= end_year_in) LOOP
+    display_total_sales(l_current_year);
+    l_current_year := l_current_year + 1;
+  END LOOP;
 END display_multiple_years;
 /
 
@@ -60,16 +52,12 @@ END display_multiple_years;
 Цикл FOR существует в двух формах: числовой и курсорной. В числовых циклах FOR задается начальное и конечное целочисленные значения, 
 а PL/SQL перебирает все промежуточные значения, после чего завершает цикл.
 */
-CREATE OR REPLACE PROCEDURE display_multiple_years (
-   start_year_in IN PLS_INTEGER
- , end_year_in   IN PLS_INTEGER
-)
-IS
+CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
+                                                   end_year_in   IN PLS_INTEGER) IS
 BEGIN
-   FOR l_current_year IN start_year_in .. end_year_in
-   LOOP
-      display_total_sales (l_current_year);
-   END LOOP;
+  FOR l_current_year IN start_year_in .. end_year_in LOOP
+    display_total_sales(l_current_year);
+  END LOOP;
 END display_multiple_years;
 /
 
@@ -77,19 +65,27 @@ END display_multiple_years;
 Курсорная форма цикла FOR имеет аналогичную базовую структуру, но вместо границ
 числового диапазона в ней задается курсор или конструкция SELECT:
 */
-CREATE OR REPLACE PROCEDURE display_multiple_years (
-   start_year_in IN PLS_INTEGER
- , end_year_in   IN PLS_INTEGER
-)
-IS
+CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
+                                                   end_year_in   IN PLS_INTEGER) IS
 BEGIN
-   FOR sales_rec IN (
-      SELECT *
-        FROM sales_data
-       WHERE year BETWEEN start_year_in AND end_year_in)
-   LOOP
-   -- Процедуре передается неявно объявленная запись с типом sales_data%ROWTYPE...
-      display_total_sales (sales_rec.year);
-   END LOOP;
+  FOR sales_rec IN (SELECT *
+                      FROM sales_data
+                     WHERE year BETWEEN start_year_in AND end_year_in) LOOP
+    -- Процедуре передается неявно объявленная запись с типом sales_data%ROWTYPE...
+    display_total_sales(sales_rec.year);
+  END LOOP;
+END display_multiple_years;
+/
+
+CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
+                                                   end_year_in   IN PLS_INTEGER) IS
+  cursor c1 is
+    SELECT *
+      FROM sales_data
+     WHERE year BETWEEN start_year_in AND end_year_in;
+BEGIN
+  FOR sales_rec IN c1 LOOP
+    display_total_sales(sales_rec.year);
+  END LOOP;
 END display_multiple_years;
 /
