c35f2081b7189df7f2f615b83a359b7ec0c4ca6a
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2022-03-26 16:45:43 +0300

    Update loopExamples.sql

--- Scripts/Oracle/Snippets/loopExamples.sql
+++ Scripts/Oracle/Snippets/loopExamples.sql
@@ -19,7 +19,7 @@ END display_total_sales;
 
 /*
 Простой цикл начинается с ключевого слова LOOP и завершается командой END LOOP.
-Выполнение цикла прерывается при выполнении команды EXIT WHEN.
+Выполнение цикла прерывается при выполнении команды EXIT WHEN или EXIT.
 */
 CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
                                                    end_year_in   IN PLS_INTEGER) IS
@@ -29,6 +29,22 @@ BEGIN
     EXIT WHEN l_current_year > end_year_in;
     display_total_sales(l_current_year);
     l_current_year := l_current_year + 1;
+  END LOOP;
+END display_multiple_years;
+/
+
+CREATE OR REPLACE PROCEDURE display_multiple_years(start_year_in IN PLS_INTEGER,
+                                                   end_year_in   IN PLS_INTEGER) IS
+  l_current_year PLS_INTEGER := start_year_in;
+BEGIN
+  LOOP
+    IF l_current_year > end_year_in 
+      THEN
+        EXIT;
+    ELSE
+      display_total_sales(l_current_year);
+      l_current_year := l_current_year + 1;
+    END IF;
   END LOOP;
 END display_multiple_years;
 /
