3814f8f15042747242d832002f2dfd065dea59a2
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2023-10-10 18:10:23 +0300

    Update ora_dba_live_sql_questions.sql

--- Scripts/Oracle/ora_dba_live_sql_questions.sql
+++ Scripts/Oracle/ora_dba_live_sql_questions.sql
@@ -3,19 +3,23 @@
 ==================
     Вопросы
 ==================
--- Выпрос №1 
+-- Вопрос №1
+-- Используя таблицы EMP и DEPT схемы SCOTT выведите информацию по всем департаментам и их служащим.
+select * from scott.dept left join scott.emp using(deptno);
+
+-- Вопрос №1 
 -- Используя таблицы CUSTOMERS и ORDERS схемы OE выведите всех клиентов которые не имеют заказов.
 
--- Выпрос №1 
+-- Вопрос №1 
 -- Используя таблицу hr.employees найдите всторую самую высокую зарплату сотрудника
 
--- Выпрос №2
+-- Вопрос №2
 -- Используя таблицу hr.employees найдите самых высокооплачиваемых сотрудников в каждом департаменте
 
--- Выпрос №3
+-- Вопрос №3
 -- Используя таблицу hr.employees выведете сотрудников с чётным порядковым номером (employee_id)
 
--- Выпрос №4
+-- Вопрос №4
 -- Выведете все заказы сделанные в Лондоне с суммой заказа больше 100.
 -- Используйте таблицы customers,stores,orders,order_items схемы CO
 -- Итоговый набор полей: 
@@ -24,11 +28,15 @@
 ==================
     Ответы
 ==================
--- Выпрос №1 
+-- Вопрос №1
+-- Используя таблицы EMP и DEPT схемы SCOTT выведите информацию по всем департаментам и их служащим.
+select * from scott.dept left join scott.emp using(deptno);
+
+-- Вопрос №1 
 -- Используя таблицы CUSTOMERS и ORDERS схемы OE выведите всех клиентов которые не имеют заказов.
 select * from oe.customers c where not exists (select 1 from oe.orders o where o.customer_id = c.customer_id); 
 
--- Выпрос №1 
+-- Вопрос №1 
 -- Используя таблицу hr.employees найдите всторую самую высокую зарплату сотрудника
 -- вариант 1
 select max(salary) from hr.employees e where e.salary not in (select max(salary) from hr.employees);
@@ -36,18 +44,18 @@ select max(salary) from hr.employees e where e.salary not in (select max(salary)
 with t as (select row_number() over (order by salary desc) as rn, e.salary from hr.employees e order by e.salary desc)
     select * from t where rn=2;
 
--- Выпрос №2
+-- Вопрос №2
 -- Используя таблицу hr.employees найдите самых высокооплачиваемых сотрудников в каждом департаменте
 -- вариант 1
 select * from hr.employees e where (department_id, salary) in (select department_id, max(salary) as salary from hr.employees group by department_id) order by department_id;
 -- вариант 2
 select * from hr.employees e where salary=(select max(salary) from hr.employees where department_id = e.department_id) order by department_id;
 
--- Выпрос №3
+-- Вопрос №3
 -- Используя таблицу hr.employees выведете сотрудников с чётным порядковым номером (employee_id)
 select * from hr.employees e where mod(employee_id, 2) = 0;
 
--- Выпрос №4
+-- Вопрос №4
 -- Выведете все заказы сделанные в Лондоне с суммой заказа больше 100.
 -- Используйте таблицы customers,stores,orders,order_items схемы CO
 -- Итоговый набор полей: 
