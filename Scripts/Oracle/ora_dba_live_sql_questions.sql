cd3322135b8dead8541cacded68dbef6092e54ab
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2023-10-11 12:21:49 +0300

    Update ora_dba_live_sql_questions.sql

--- Scripts/Oracle/ora_dba_live_sql_questions.sql
+++ Scripts/Oracle/ora_dba_live_sql_questions.sql
@@ -1,38 +1,36 @@
 -- Вопросы для собеседования на https://livesql.oracle.com
--- Практическая часть собеседования включает в себя написание sql запросов, для её выполнения 
--- необходимо зарегистрироваться на сервисе https://livesql.oracle.com
+
 ==================
     Вопросы
 ==================
--- Выпрос №4
--- Выведете топ три заказа (по сумме) для каждого клиента.
--- Используйте таблицы ORDERS,ORDER_ITEM схемы CO
--- Итоговый набор полей: 
--- клиент (customer_id), заказ (order_id), сумма_заказа (order_total)
-
 -- Вопрос №1
 -- Используя таблицу EMPLOYEES схемы HR найдите топ три зарплаты в комапании.
 
--- Вопрос №1
+-- Вопрос №2
 -- Используя таблицы EMP и DEPT схемы SCOTT выведите информацию по всем департаментам и их служащим.
 
--- Вопрос №1 
+-- Вопрос №3
 -- Используя таблицы CUSTOMERS и ORDERS схемы OE выведите всех клиентов которые не имеют заказов.
 
--- Вопрос №1 
--- Используя таблицу hr.employees найдите всторую самую высокую зарплату сотрудника
-
--- Вопрос №2
--- Используя таблицу hr.employees найдите самых высокооплачиваемых сотрудников в каждом департаменте
-
--- Вопрос №3
--- Используя таблицу hr.employees выведете сотрудников с чётным порядковым номером (employee_id)
-
 -- Вопрос №4
--- Выведете все заказы сделанные в Лондоне с суммой заказа больше 100.
--- Используйте таблицы customers,stores,orders,order_items схемы CO
+-- Используя таблицу EMPLOYEES схемы HR найдите самых высокооплачиваемых сотрудников в каждом департаменте
+
+-- Вопрос №5
+-- Используя таблицу EMPLOYEES схемы HR выведете сотрудников с чётным порядковым номером (employee_id)
+
+-- Выпрос №6
+-- Выведете топ три заказа (по сумме) для каждого клиента.
+-- Используйте таблицы ORDERS, ORDER_ITEM схемы CO
+-- Итоговый набор полей: 
+-- клиент (customer_id), заказ (order_id), сумма_заказа (order_total)
+-- сортрировка по customer_id от меньшего к большему
+
+-- Вопрос №7
+-- Выведете все заказы сделанные в Лондоне с суммой заказа больше либо равной 100.
+-- Используйте таблицы CUSTOMERS, STORES, ORDERS, ORDER_ITEMS схемы CO
 -- Итоговый набор полей: 
 -- номер_заказа (order_id), имя_клиента (full_name), имя_магазина (store_name), сумма_заказа (order_total)
+-- сортрировка по order_total от большего к меньшему 
     
 ==================
     Ответы
@@ -41,50 +39,43 @@
 -- Используя таблицу EMPLOYEES схемы HR найдите топ три зарплаты в комапании.
 select distinct(salary) from hr.employees e order by salary desc fetch first 3 rows only;
 
--- Вопрос №1
+-- Вопрос №2
 -- Используя таблицы EMP и DEPT схемы SCOTT выведите информацию по всем департаментам и их служащим.
 select * from scott.dept left join scott.emp using(deptno);
 
--- Вопрос №1 
+-- Вопрос №3
 -- Используя таблицы CUSTOMERS и ORDERS схемы OE выведите всех клиентов которые не имеют заказов.
 select * from oe.customers c where not exists (select 1 from oe.orders o where o.customer_id = c.customer_id); 
 
--- Вопрос №1 
--- Используя таблицу hr.employees найдите всторую самую высокую зарплату сотрудника
--- вариант 1
-select max(salary) from hr.employees e where e.salary not in (select max(salary) from hr.employees);
--- вариант 2
-with t as (select row_number() over (order by salary desc) as rn, e.salary from hr.employees e order by e.salary desc)
-    select * from t where rn=2;
-
--- Вопрос №2
--- Используя таблицу hr.employees найдите самых высокооплачиваемых сотрудников в каждом департаменте
+-- Вопрос №4
+-- Используя таблицу EMPLOYEES схемы HR найдите самых высокооплачиваемых сотрудников в каждом департаменте
 -- вариант 1
 select * from hr.employees e where (department_id, salary) in (select department_id, max(salary) as salary from hr.employees group by department_id) order by department_id;
 -- вариант 2
 select * from hr.employees e where salary=(select max(salary) from hr.employees where department_id = e.department_id) order by department_id;
 
--- Вопрос №3
--- Используя таблицу hr.employees выведете сотрудников с чётным порядковым номером (employee_id)
+-- Вопрос №5
+-- Используя таблицу EMPLOYEES схемы HR выведете сотрудников с чётным порядковым номером (employee_id)
 select * from hr.employees e where mod(employee_id, 2) = 0;
 
--- Выпрос №4
+-- Выпрос №6
 -- Выведете топ три заказа (по сумме) для каждого клиента.
--- Используйте таблицы ORDERS,ORDER_ITEM схемы CO
+-- Используйте таблицы ORDERS, ORDER_ITEM схемы CO
 -- Итоговый набор полей: 
 -- клиент (customer_id), заказ (order_id), сумма_заказа (order_total)
+-- сортрировка по customer_id от меньшего к большему
     with t (customer_id, order_id, order_total, rn) as (
 select o.customer_id, o.order_id, sum(oi.unit_price * oi.quantity) as order_total, row_number() over (partition by o.customer_id order by sum(oi.unit_price * oi.quantity) desc) as rn
     from co.orders o join co.order_items oi on o.order_id=oi.order_id
     group by o.customer_id, o.order_id)
     select * from t where rn <= 3 order by customer_id;
 
--- Вопрос №4
--- Выведете все заказы сделанные в Лондоне с суммой заказа больше 100.
--- Используйте таблицы customers,stores,orders,order_items схемы CO
+-- Вопрос №7
+-- Выведете все заказы сделанные в Лондоне с суммой заказа больше либо равной 100.
+-- Используйте таблицы CUSTOMERS, STORES, ORDERS, ORDER_ITEMS схемы CO
 -- Итоговый набор полей: 
 -- номер_заказа (order_id), имя_клиента (full_name), имя_магазина (store_name), сумма_заказа (order_total)
-
+-- сортрировка по order_total от большего к меньшему 
 select  o.order_id, c.full_name, s.store_name, sum(oi.unit_price * oi.quantity) as order_total
     from co.orders o 
     join co.stores s on o.store_id = s.store_id
