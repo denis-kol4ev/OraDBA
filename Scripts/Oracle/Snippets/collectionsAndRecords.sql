0d7ee97931858ef93084f19f3b127d0e8541bd02
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2024-04-01 20:31:21 +0300

    Update collectionsAndRecords.sql

--- Scripts/Oracle/Snippets/collectionsAndRecords.sql
+++ Scripts/Oracle/Snippets/collectionsAndRecords.sql
@@ -1,12 +1,94 @@
 -- Collections and Records
 
+-- 1. Collections 
 -- Three types of collections
 -- Nested Tables
 -- Associative array
 -- Varray    
 
+-- Nested Tables
+DECLARE
+   TYPE nested_type IS TABLE OF VARCHAR2(20);
+   v1 nested_type;
+BEGIN
+   v1 := nested_type('A','B','C','D');
+   for i in v1.first .. v1.last loop
+   dbms_output.put_line(v1(i));
+   end loop;
+END;
+/
 
+DECLARE
+   TYPE nested_type IS TABLE OF VARCHAR2(20);
+   v1 nested_type := nested_type('A','B','C','D');
+BEGIN
+   for i in v1.first .. v1.last loop
+   dbms_output.put_line(v1(i));
+   end loop;
+END;
+/
 
+-- Associative array
+DECLARE
+  TYPE assoc_array_num_type IS TABLE OF NUMBER INDEX BY PLS_INTEGER;
+  TYPE assoc_array_str_type IS TABLE OF VARCHAR2(32) INDEX BY PLS_INTEGER;
+  TYPE assoc_array_str_type2 IS TABLE OF VARCHAR2(32) INDEX BY VARCHAR2(64);
+  v1    assoc_array_num_type;
+  v2    assoc_array_str_type;
+  v3    assoc_array_str_type2;
+  v_idx varchar(20);
+BEGIN
+  v1(1) := 10;
+  v1(2) := 20;
+  v1(3) := 30;
+
+  for i in v1.first .. v1.last loop
+    dbms_output.put_line(v1(i));
+  end loop;
+
+  v2(1) := 'A';
+  v2(2) := 'B';
+  v2(3) := 'C';
+
+  for i in v2.first .. v2.last loop
+    dbms_output.put_line(v2(i));
+  end loop;
+
+  v3('Ottawa') := 'Canada';
+  v3('Washington') := 'USA';
+  v3('Moscow') := 'Russia';
+
+  v_idx := v3.first;
+  while (v_idx is not null) loop
+    dbms_output.put_line(v3(v_idx));
+    v_idx := v3.next(v_idx);
+  end loop;
+END;
+/
+
+-- Varray 
+DECLARE
+   TYPE varray_type IS VARRAY(5) OF INTEGER;
+   v1 varray_type;
+BEGIN
+   v1 := varray_type(10, 20, 40, 80, 160);
+   for i in v1.first .. v1.last loop
+   dbms_output.put_line(v1(i));
+   end loop;
+END;
+/
+
+DECLARE
+   TYPE varray_type IS VARRAY(5) OF INTEGER;
+   v1 varray_type := varray_type(10, 20, 40, 80, 160);
+BEGIN
+   for i in v1.first .. v1.last loop
+   dbms_output.put_line(v1(i));
+   end loop;
+END;
+/
+
+-- 2. Records 
 -- Cursor-based record
 -- record_name cursor_name%ROWTYPE;
 DECLARE
