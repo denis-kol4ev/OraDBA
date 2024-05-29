5936e32b013e983bdd72d059357a782bc332d91d
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2024-05-29 22:36:11 +0300

    Update Проверка на существование.sql

--- Scripts/Oracle/Snippets/Проверка на существование.sql
+++ Scripts/Oracle/Snippets/Проверка на существование.sql
@@ -48,6 +48,35 @@ if isRoleExist(v_role_name) then
  v_sqlstr_cr := 'create role ' || v_role_name;
     dbms_output.put_line('v_sqlstr_cr = ' || v_sqlstr_cr);
     --  execute immediate v_sqlstr_cr;
-
   end if;
+end;
+  
+--- Функция проверки существования пользователя
+declare
+  type users_type is table of dba_users.username%type;
+  v_users users_type := users_type('SYS', 'AAA', 'SYSTEM', 'BBB', 'DBSNMP');
+
+  function isUserExist(v_user_name varchar2) return boolean as
+    v_username_check dba_users.username%type;
+  begin
+    begin
+      select username
+        into v_username_check
+        from dba_users
+       where username = v_user_name;
+    exception
+      when others then
+        v_username_check := null;
+    end;
+    return v_username_check is not null;
+  end isUserExist;
+  
+begin
+  for i in v_users.first .. v_users.last loop
+    if isUserExist(v_users(i)) then
+      dbms_output.put_line('User ' || v_users(i) || ' is exist');
+    else
+      dbms_output.put_line('User ' || v_users(i) || ' does not exist');
+    end if;
+  end loop;
 end;
