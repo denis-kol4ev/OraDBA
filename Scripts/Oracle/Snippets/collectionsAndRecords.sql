02403b3cc7d373e50fa1d8a9550eab97e78c3b95
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2024-07-12 17:31:27 +0300

    Update collectionsAndRecords.sql

--- Scripts/Oracle/Snippets/collectionsAndRecords.sql
+++ Scripts/Oracle/Snippets/collectionsAndRecords.sql
@@ -21,6 +21,44 @@ BEGIN
 END;
 /
 
+-- Comparing Nested Tables with SQL Multiset Conditions
+DECLARE 
+  TYPE nested_typ IS TABLE OF NUMBER; 
+  nt1 nested_typ := nested_typ(1,2,3); 
+  nt2 nested_typ := nested_typ(3,2,1); 
+  nt3 nested_typ := nested_typ(2,3,1,3); 
+  nt4 nested_typ := nested_typ(1,2,4); 
+ 
+  PROCEDURE testify ( 
+    truth BOOLEAN := NULL, 
+    quantity NUMBER := NULL 
+  ) IS 
+  BEGIN 
+    IF truth IS NOT NULL THEN 
+      DBMS_OUTPUT.PUT_LINE ( 
+        CASE truth 
+           WHEN TRUE THEN 'True' 
+           WHEN FALSE THEN 'False' 
+        END 
+      ); 
+    END IF; 
+    IF quantity IS NOT NULL THEN 
+        DBMS_OUTPUT.PUT_LINE(quantity); 
+    END IF; 
+  END; 
+BEGIN 
+  testify(truth => (nt1 IN (nt2,nt3,nt4)));        -- condition 
+  testify(truth => (nt1 SUBMULTISET OF nt3));      -- condition 
+  testify(truth => (nt1 NOT SUBMULTISET OF nt4));  -- condition 
+  testify(truth => (4 MEMBER OF nt1));             -- condition 
+  testify(truth => (nt3 IS A SET));                -- condition 
+  testify(truth => (nt3 IS NOT A SET));            -- condition 
+  testify(truth => (nt1 IS EMPTY));                -- condition 
+  testify(quantity => (CARDINALITY(nt3)));         -- function 
+  testify(quantity => (CARDINALITY(SET(nt3))));    -- 2 functions 
+END; 
+/
+
 DECLARE
    TYPE nested_type IS TABLE OF VARCHAR2(20);
    v1 nested_type := nested_type('A','B','C','D');
