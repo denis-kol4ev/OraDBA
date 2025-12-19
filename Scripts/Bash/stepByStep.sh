09c5eb155363b3dc7b721d3c64735d43f0b6b4ff
denis-kol4ev <59415007+denis-kol4ev@users.noreply.github.com>
2025-12-20 00:48:18 +0300

    Update stepByStep.sh

--- Scripts/Bash/stepByStep.sh
+++ Scripts/Bash/stepByStep.sh
@@ -1,17 +1,47 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]; then
-    START_STEP=1
-else 
-    START_STEP=$1
+Help()
+{
+   echo "Script for cloning a production database into a test environment."
+   echo
+   echo "Syntax: $0 [-h|-s|]"
+   echo "options:"
+   echo "-h   Print this Help."
+   echo "-s   Start step at what should the script be run."
+   echo
+}
+ 
+START_STEP=1
+
+while getopts ":hs:" option; do
+   case $option in
+      h)
+         Help
+         exit;;
+      s)
+         START_STEP=$OPTARG;;
+      :)
+        echo "Error: option -$OPTARG requires an argument (integer)"
+        exit;;
+     \?)
+         echo "Error: invalid option, use -h for help"
+         exit;;
+   esac
+done
+
+if ! [[ ${START_STEP} =~ ^[0-9]+$ ]]; then
+    echo "Error: -s argument must be an integer"
+    exit 1
 fi
+
+echo "Start script from step: ${START_STEP}"
 
 CURR_STEP=1
 if [[ ${CURR_STEP} -eq ${START_STEP} ]]; then
     echo "Do work for step " ${CURR_STEP}
     ((START_STEP+=1))
 else
-    echo "Step " ${CURR_STEP} " skipped"
+    echo "Step" ${CURR_STEP} "skipped"
 fi
 
 CURR_STEP=2
@@ -19,7 +49,7 @@ if [[ ${CURR_STEP} -eq ${START_STEP} ]]; then
     echo "Do work for step " ${CURR_STEP}
     ((START_STEP+=1))
 else
-    echo "Step " ${CURR_STEP} " skipped"
+    echo "Step" ${CURR_STEP} "skipped"
 fi
 
 CURR_STEP=3
@@ -27,5 +57,5 @@ if [[ ${CURR_STEP} -eq ${START_STEP} ]]; then
     echo "Do work for step " ${CURR_STEP}
     ((START_STEP+=1))
 else
-    echo "Step " ${CURR_STEP} " skipped"
+    echo "Step" ${CURR_STEP} "skipped"
 fi
